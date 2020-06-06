a895a3550fe1ef35982d9fdc098669a217fbd5b9
Mateus Barbosa <mateusbarbosa@infojr.com.br>
2020-06-06 13:47:12 -0300

    adds missing code for add, list and remove repositories

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
   async function handleAddRepository() {
-    // TODO
+    const repository = {
+      title: "ConceitosNodeJs-GoStack " + Date.now(),
+      url: "https://github.com/mbmachado/ConceitosNodeJS-GoStack",
+      techs: ["NodeJs", "Jest", "ExpressJS"]
+    }
+
+    const { data } = await api.post('repositories', repository);
+
+    setRepositories([...repositories, data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    
+    const i = repositories.findIndex(obj => obj.id = id);
+    const newRepositories = Array.from(repositories);
+    newRepositories.splice(i, 1);
+    setRepositories(newRepositories);
   }
+
+  useEffect(() => {
+    api.get('repositories').then(({ data }) => {
+      setRepositories(data);
+    });
+  }, []);
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
